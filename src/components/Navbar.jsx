ebfe6936ab0619fb401b55f3f0c52c97b12daa13
mahtoaman <itismyacnt@gmail.com>
2023-05-06 11:23:39 +0530

    warning removed from navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Navbar() {
+export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a className="navbar-brand" href="#">
+      <a className="navbar-brand" href="www.tgtc.in">
         T G T C
       </a>
       <button
@@ -20,22 +20,22 @@ function Navbar() {
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item active">
-            <a className="nav-link" href="#">
+            <a className="nav-link" href="/">
               Home
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
+            <a className="nav-link" href="services">
               Services
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
+            <a className="nav-link" href="about">
               About
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
+            <a className="nav-link" href="contact-us">
               Contact Us
             </a>
           </li>
@@ -44,5 +44,3 @@ function Navbar() {
     </nav>
   );
 }
-
-export default Navbar;
